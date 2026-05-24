5273915a54237bbbd08746c598b82ca1c3cb572c
Maged Ibrahim <magedibra@yahoo.com>
2026-05-24 22:53:27 +0300

    fix: a11y

--- src/app/(home)/components/input-group-button.tsx
+++ src/app/(home)/components/input-group-button.tsx
@@ -27,9 +27,9 @@ export function InputGroupButtonExample() {
 			<InputGroup>
 				<InputGroupInput id="input-secure-19" />
 				<Popover>
-					<PopoverTrigger
-						render={
-							<InputGroupAddon>
+					<InputGroupAddon>
+						<PopoverTrigger
+							render={
 								<InputGroupButton
 									variant="secondary"
 									size="icon-sm"
@@ -37,9 +37,9 @@ export function InputGroupButtonExample() {
 								>
 									<InfoIcon />
 								</InputGroupButton>
-							</InputGroupAddon>
-						}
-					/>
+							}
+						/>
+					</InputGroupAddon>
 					<PopoverContent
 						align="start"
 						alignOffset={10}
@@ -49,7 +49,7 @@ export function InputGroupButtonExample() {
 						<p>You should not enter any sensitive information on this site.</p>
 					</PopoverContent>
 				</Popover>
-				<InputGroupAddon className="pl-1! text-muted-foreground">
+				<InputGroupAddon className="-ml-1.5 text-muted-foreground">
 					https://
 				</InputGroupAddon>
 				<InputGroupAddon align="inline-end">
